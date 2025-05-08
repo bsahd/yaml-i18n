d1e106dde871c0c04f5594e2e3529adb1cb76c45
bsahd <193844131+bsahd@users.noreply.github.com>
2025-05-08 16:30:10 +0900

    add config file and remove config from document

--- conv.js
+++ conv.js
@@ -2,9 +2,10 @@ import { load } from "js-yaml";
 import fs from "fs";
 import path from "path";
 
+const config = load(fs.readFileSync("./doc.config.yml", "utf8"));
 for (const filename of fs.globSync("src/**/*")) {
 	const doc = load(fs.readFileSync(filename, "utf8"));
-	for (const language of doc.languages) {
+	for (const language of config.languages) {
 		let langdoc = "";
 		for (const line of doc.lines) {
 			langdoc +=
@@ -12,7 +13,7 @@ for (const filename of fs.globSync("src/**/*")) {
 					? line
 					: language in line
 					? line[language]
-					: "[UNTRANSLATED]" + line[doc.mainlanguage]) + "\n";
+					: "[UNTRANSLATED]" + line[config.mainlanguage]) + "\n";
 		}
 		fs.mkdirSync(`dist/${language}/${path.dirname(filename.slice(4))}`, {
 			recursive: true,
